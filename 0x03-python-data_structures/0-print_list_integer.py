730b7aab39828759608020d031458fc04af5e6d7
jawadzizo <jadosss99@gmail.com>
2024-01-05 10:51:37 -0800

    added 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        for element in my_list:
-            print("{}" .format(element))
+def print_list_integer(my_list=[]):
+    for element in my_list:
+        print("{}" .format(element))
