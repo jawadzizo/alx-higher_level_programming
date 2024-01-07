dbb18a576ef28dfd351c660e66cbd61a4dfa46ba
jawadzizo <jadosss99@gmail.com>
2024-01-07 10:13:42 -0800

    updated 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,11 +5,13 @@ if __name__ == "__main__":
 
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
+        exit(1)
 
-    if sys.argv[2] != '+' and sys.argv[2] != '-' and sys.argv[2] != '*' and sys.argv[2] != '/':
+    operations = "+-*/"
+
+    if sys.argv[2] not in operations:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit (1)
+        exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
