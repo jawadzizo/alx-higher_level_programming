5d9771c45597e945b98bb7c7a813697458ab1f32
jawadzizo <jadosss99@gmail.com>
2024-01-10 12:27:55 -0800

    added 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    new = {k: v*2 for k, v in a_dictionary.items()}
+    new = {k: v*2 for k, v in a_dictionary.items() if isinstance(v, int)}
     return new 
