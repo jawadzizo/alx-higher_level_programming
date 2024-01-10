1163fe2ced501d6b0fe3930374bde424ec3a3ce5
jawadzizo <jadosss99@gmail.com>
2024-01-10 12:49:59 -0800

    added 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if isinstance(a_dictionary, (list, set, tuple, dict)):
-        return max(a_dictionary)
+    if isinstance(a_dictionary, (dict)) and len(a_dictionary):
+        max_key = ""
+        max_value = ""
+        for k, v in a_dictionary.items():
+            if isinstance(max_value, str):
+                max_value = v
+            if v > max_value:
+                max_value = v
+                max_key = k
+
+        return max_key
