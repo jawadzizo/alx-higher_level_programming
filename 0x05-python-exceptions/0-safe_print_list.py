fc03601383773e94b55b4445e85725519488ea29
jawadzizo <jadosss99@gmail.com>
2024-01-23 06:28:55 -0800

    updated 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 
 def safe_print_list(my_list=[], x=0):
     printed = 0
-    for i in range(x - 1):
+    for i in range(x):
         try:
             print("{0}" .format(my_list[i]), end="")
             printed += 1
