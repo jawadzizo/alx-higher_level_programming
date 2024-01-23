f828204dedfe4a33b7096a475e43d4a68144b20c
jawadzizo <jadosss99@gmail.com>
2024-01-23 13:45:30 -0800

    added 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a file containing all methods and fields of the class \"Square\""""
+
+
+class Square:
+    """the first version of the class \"Square\""""
+    def __init__(self, size):
+        """the __init__ method to define some object fields"""
+        self.__size = size
