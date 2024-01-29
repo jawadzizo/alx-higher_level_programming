fabd248e6280b5eb334881612364c140b5e4c42a
jawadzizo <jadosss99@gmail.com>
2024-01-29 12:39:38 -0800

    added 3-rectangle.py

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""a file containing all methods and fields of the class \"Rectangle\""""
+
+
+class Rectangle:
+    """the 4th version of the class \"Rectangle\""""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """returns the width of the Rectangle class"""
+        return self.__width
+
+    @property
+    def height(self):
+        """returns the height of the Rectangle class"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """sets the rectangle's width after validating some conditions"""
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """sets the rectangle's height after validating some conditions"""
+        if isinstance(value, int) is False:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width + self.__height) * 2)
+
+    def __str__(self):
+        """returns a formated rectangle using '#'"""
+        text = ""
+        if self.__width == 0 or self.__height == 0:
+            return text
+        for i in range(self.__height):
+            text += ("#" * self.__width)
+            if i != self.__height - 1:
+                text += "\n"
+        return text
