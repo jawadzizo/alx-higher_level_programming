938b676b74f0877dc7a0f25295f9a4c779e2d9ab
jawadzizo <jadosss99@gmail.com>
2024-02-06 13:37:20 -0800

    added 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""A module that returns the dictionary of a class instance"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """initialize instance's attributes"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns the dictionary of a class instance based on given attrs"""
+
+        if attrs is None:
+            return (self.__dict__)
+
+        dictionary = dict()
+        self_dict = self.__dict__
+
+        for k, v in self_dict.items():
+            if k in attrs:
+                dictionary[k] = self_dict[k]
+
+        return (dictionary)
+
+    def reload_from_json(self, json):
+        """reload an instance attribues based on a json"""
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
