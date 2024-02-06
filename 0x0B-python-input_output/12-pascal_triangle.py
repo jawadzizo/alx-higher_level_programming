c19d857ee84ddbb610ae90b09ae6056d30ec9c92
jawadzizo <jadosss99@gmail.com>
2024-02-06 14:34:53 -0800

    added 12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""A module that returns a Pascal Triangle using a nested list"""
+
+
+def pascal_triangle(n):
+    pascal = []
+    length = 1
+
+    if isinstance(n, int) is False or n <= 0:
+        return (pascal)
+
+    for i in range(n):
+        array = []
+
+        for j in range(length):
+
+            if i == 0 or len(array) == 0 or len(array) == len(pascal[i - 1]):
+                element = 1
+            else:
+                element = pascal[i-1][len(array)] + pascal[i-1][len(array) - 1]
+
+            array.append(element)
+
+        length += 1
+        pascal.append(array)
+
+    return (pascal)
