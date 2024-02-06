ebec035fa9e1d8fc270504514dad1e8c962b0322
jawadzizo <jadosss99@gmail.com>
2024-02-06 13:06:21 -0800

    added 8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A module that returns the dictionary of a class instance"""
+
+
+def class_to_json(obj):
+    """returns the dictionary of a class instance"""
+
+    return (obj.__dict__)
