516a9af56c7ca08ff840b197f8826fdf6d2b2605
jawadzizo <jadosss99@gmail.com>
2024-02-11 07:29:06 -0800

    added 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that prints a sorted list
+"""
+
+
+class MyList(list):
+    """"""
+
+    def print_sorted(self):
+        """prints a sorted list"""
+
+        print(sorted(self))
