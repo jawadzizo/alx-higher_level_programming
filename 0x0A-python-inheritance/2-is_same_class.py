42d940620d8c57341cbd84e9d0919ec7aaf78870
jawadzizo <jadosss99@gmail.com>
2024-02-11 08:09:48 -0800

    added 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""checks if 'obj' is of class 'a_class'
+"""
+
+
+def is_same_class(obj, a_class):
+    """checks if 'obj' is of class 'a_class'"""
+
+    return type(obj) is a_class
