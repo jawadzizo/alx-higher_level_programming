09be0449cf36f6d6f04938f3b8f295fe81641895
jawadzizo <jadosss99@gmail.com>
2024-02-12 11:20:03 -0800

    added validation in models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,10 +12,10 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """The constructor method for Rectangle class's instances"""
 
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
         super().__init__(id)
 
@@ -42,20 +42,43 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """The setter method of width attribute"""
+
+        if isinstance(width, int) is False or isinstance(width, bool):
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+
         self.__width = width
 
     @height.setter
     def height(self, height):
         """The getter method of height attribute"""
+
+        if isinstance(height, int) is False or isinstance(height, bool):
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+
         self.__height = height
 
     @x.setter
     def x(self, x):
         """The getter method of x attribute"""
+
+        if isinstance(x, int) is False or isinstance(x, bool):
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+
         self.__x = x
 
     @y.setter
     def y(self, y):
         """The getter method of y attribute"""
+
+        if isinstance(y, int) is False or isinstance(y, bool):
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
 
         self.__y = y
