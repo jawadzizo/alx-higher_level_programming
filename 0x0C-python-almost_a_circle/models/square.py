ac407690e203684c08f96b77ae4375aed54f32c3
jawadzizo <jadosss99@gmail.com>
2024-02-13 10:41:46 -0800

    added update() in models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -31,3 +31,28 @@ class Square(Rectangle):
 
         self.width = size
         self.height = size
+
+    def update(self, *args, **kwargs):
+        """updates the attributes of Square's instances"""
+
+        if len(args) > 0:
+
+            attributes = [0, 1, 2, 3]
+
+            attributes[0] = args[0] if len(args) > 0 else self.id
+            attributes[1] = args[1] if len(args) > 1 else self.size
+            attributes[2] = args[2] if len(args) > 2 else self.x
+            attributes[3] = args[3] if len(args) > 3 else self.y
+
+            super().__init__(attributes[1], attributes[1], attributes[2], attributes[3], attributes[0])
+
+        elif len(kwargs) > 0:
+
+            attributes = {}
+
+            attributes["id"] = kwargs["id"] if "id" in kwargs else self.id
+            attributes["size"] = kwargs["size"] if "size" in kwargs else self.size
+            attributes["x"] = kwargs["x"] if "x" in kwargs else self.x
+            attributes["y"] = kwargs["y"] if "y" in kwargs else self.y
+
+            super().__init__(attributes["size"], attributes["size"], attributes["x"], attributes["y"], attributes["id"])
