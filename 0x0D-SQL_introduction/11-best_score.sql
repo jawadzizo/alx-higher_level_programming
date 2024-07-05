94a92fdfd80c886dc1d85f1a6ed054c2f9cf454f
jawadzizo <jadosss99@gmail.com>
2024-07-05 15:18:40 -0700

    add 11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- lists all rows in the table "second_table" with score >= 10
-SELECT score, name FROM second_table WHERE id >= 10
+SELECT score, name FROM second_table WHERE score >= 10
 ORDER BY score DESC;
