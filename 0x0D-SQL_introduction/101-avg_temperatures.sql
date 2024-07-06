4a3ff0db756e3993a8e5b930ed994ca6f76d8ba8
jawadzizo <jadosss99@gmail.com>
2024-07-05 17:02:05 -0700

    add 101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,4 @@
 -- show the average temperature by city, ordered in descending order
-SOURCE temperatures.sql;
 SELECT city, AVG(value) AS avg_temp FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
