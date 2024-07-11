1e33645dc53dc5fbc8a14cdf542692db1a1caada
jawadzizo <jadosss99@gmail.com>
2024-07-11 09:19:10 -0700

    add 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- lists tv show with their genres, ordered
-SELECT tv_shows.title as title,
+SELECT tv_shows.title AS title,
         tv_show_genres.genre_id AS genre_id
 FROM tv_shows INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
