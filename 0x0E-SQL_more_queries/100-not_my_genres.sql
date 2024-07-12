66d0a4eaa9d3d758dd6b2cbf8bbff5670fbfb149
jawadzizo <jadosss99@gmail.com>
2024-07-12 05:09:25 -0700

    add 100-not_my_genres.sql

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,18 @@
+-- lists the genres dexter doesn't belong to
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+
+EXCEPT
+
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE title = "Dexter"
+ORDER BY name;
