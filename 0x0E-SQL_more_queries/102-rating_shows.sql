6c518a2e8568c8f12aa7c1b60ddc3805ec5b8088
jawadzizo <jadosss99@gmail.com>
2024-07-12 05:57:34 -0700

    add 102-rating_shows.sql

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- lists the tv shows and the sum of their rating
+SELECT tv_shows.title,
+    SUM(rate) AS rating
+FROM tv_shows
+INNER JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
