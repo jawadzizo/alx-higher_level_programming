f0519fe28a941466a7a5119f0622862a1535e375
jawadzizo <jadosss99@gmail.com>
2024-07-21 16:07:25 -0700

    add 2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+    constructor (w, h) {
+        this.width = w;
+        this.height = h;
+
+        if (w === undefined || w <= 0 ||
+            h === undefined || h <= 0) {
+            return {};
+        }
+    }
+};
