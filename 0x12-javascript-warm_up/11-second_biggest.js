fd003a7ef6478a2ad626df729508459b9e0ea3ad
jawadzizo <jadosss99@gmail.com>
2024-07-21 04:13:44 -0700

    add 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 const args = process.argv;
 
 if (args[2] === undefined || args[3] === undefined) {
-    console.log('1');
+    console.log('0');
 } else {
     let array = args.slice(2).map((x) => Number(x));
     array = array.sort((a, b) => b - a);
