3990c17dcb50092c9f018a1e680bf32648d942ef
jawadzizo <jadosss99@gmail.com>
2024-08-05 05:08:44 -0700

    add 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""lists all states in the given database that starts with 'N'"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """the main function of the module"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect("localhost", username, password, database)
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY id;")
+    states = c.fetchall()
+
+    for state in states:
+        print(state)
+
+
+if __name__ == "__main__":
+    main()
