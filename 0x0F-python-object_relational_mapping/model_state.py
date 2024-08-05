686927bada2beb3b9b24a5255debe96a06c569e9
jawadzizo <jadosss99@gmail.com>
2024-08-05 09:56:38 -0700

    add model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""the base module for the database classes"""
+
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """points to the states table"""
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
