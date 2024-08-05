5b87fc8765b43efd0adb7dd320ce9c271ea5962f
jawadzizo <jadosss99@gmail.com>
2024-08-05 12:21:41 -0700

    add 10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""lists the first state in the database using states.id
+"""
+import sys
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    """the main function of the module"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    searched_state = sys.argv[4]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, database))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == searched_state).first()
+    print(state.id if state else "Not found")
+
+
+if __name__ == "__main__":
+    main()
