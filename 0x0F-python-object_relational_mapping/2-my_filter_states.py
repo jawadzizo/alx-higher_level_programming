4bee5eb66fa038dd1235f026b89a9c93769c9bb2
jawadzizo <jadosss99@gmail.com>
2024-08-05 07:55:29 -0700

    add 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,13 +10,16 @@ def main():
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    searched_state = sys.argv[4]
 
     db = MySQLdb.connect("localhost", username, password, database)
     c = db.cursor()
-    c.execute("SELECT * FROM states WHERE name='{}';" .format(sys.argv[4]))
-    state = c.fetchone()
+    c.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id;"
+              .format(searched_state))
+    states = [state for state in c.fetchall() if state[1] == searched_state]
 
-    print(state)
+    for state in states:
+        print(state)
 
 
 if __name__ == "__main__":
